c6d630e85ff12d7199eb42a3368967924d03cf5b
xrandox <xrandox17@yahoo.com>
2023-07-22 03:29:25 -0500

    tweaks

--- Automatic-Arrows/Data/Automatic-Arrows/aamenu.lua
+++ Automatic-Arrows/Data/Automatic-Arrows/aamenu.lua
@@ -2,13 +2,6 @@ local function toggleTutorial(menu)
     AA_ToggleTutorial()
 end
 
-local function isCurrentTarget(name)
-    if (name == AA.AutomaticArrows.workingCategory.DisplayName) then
-        return true
-    end
-    return false
-end
-
 local function changeTargetCategory(menu)
     local dispName = menu.Name
     for index, category in ipairs(AA.loader.visibleCategories) do
@@ -20,16 +13,16 @@ local function changeTargetCategory(menu)
     menu.Checked = true
 end
 
-local function isCurrentMax(str)
-    if (AA.AutomaticArrows.maximumTargets == str) then return true
-    else return false end
-end
-
 local function maximumTargets(menu)
     AA.AutomaticArrows.maximumTargets = menu.Name
     Storage:UpsertValue("automaticarrows", "maximumTargets", AA.AutomaticArrows.maximumTargets)
     AA_ChangeMaxArrows()
     menu.Checked = true
+end
+
+local function changeDistanceSetting(menu)
+    AA.utils.currentSetting = menu.Name
+    Storage:UpsertValue("automaticarrows", "currentSetting", menu.Name)
 end
 
 local function toggleButton(menu)
@@ -56,11 +49,16 @@ local targets = {}
 
 
 local maxTargMenu = mainMenu:Add("Change Maximum Arrows", nil, false, false, "Allows you to select how many arrows you would like to have shown at once. The arrows will point to the closest X markers")
-maxTargMenu:Add("1", maximumTargets, true, isCurrentMax("1"), "Sets the maximum number of arrows to only one")
-maxTargMenu:Add("2", maximumTargets, true, isCurrentMax("2"), "Sets the maximum number of arrows to two")
-maxTargMenu:Add("3", maximumTargets, true, isCurrentMax("3"), "Sets the maximum number of arrows to three")
-maxTargMenu:Add("4", maximumTargets, true, isCurrentMax("4"), "Sets the maximum number of arrows to four")
-maxTargMenu:Add("5", maximumTargets, true, isCurrentMax("5"), "Sets the maximum number of arrows to five")
+maxTargMenu:Add("1", maximumTargets, false, false, "Sets the maximum number of arrows to only one")
+maxTargMenu:Add("2", maximumTargets, false, false, "Sets the maximum number of arrows to two")
+maxTargMenu:Add("3", maximumTargets, false, false, "Sets the maximum number of arrows to three")
+maxTargMenu:Add("4", maximumTargets, false, false, "Sets the maximum number of arrows to four")
+maxTargMenu:Add("5", maximumTargets, false, false, "Sets the maximum number of arrows to five")
+
+local distSettingMenu = mainMenu:Add("Change Distance Calibration", nil, false, false, "Changes when the arrows transition through the different colors")
+distSettingMenu:Add("Long", changeDistanceSetting, false, false, "For markers that are separated by long distances")
+distSettingMenu:Add("Medium", changeDistanceSetting, false, false, "For markers that aren't really far apart, but aren't close")
+distSettingMenu:Add("Close", changeDistanceSetting, false, false, "For markers that are close together")
 
 local toggle = mainMenu:Add("Toggle Automatic Arrows On", toggleButton, true, (AA.AutomaticArrows.arrowsToggledOn == "true"), "This toggles Automatic Arrows on and off. Please ensure you have only the markers you want to be guided to visible.")
 
@@ -70,7 +68,7 @@ function AA_RefreshTargets()
     else
         for _, category in ipairs(AA.loader.visibleCategories) do
             if (targets[category.DisplayName] == nil) then
-                targetMenu:Add(category.DisplayName, changeTargetCategory, true, isCurrentTarget(category.DisplayName))
+                targetMenu:Add(category.DisplayName, changeTargetCategory, false, false)
                 targets[category.DisplayName] = true
             end
         end
--- Automatic-Arrows/Data/Automatic-Arrows/utils.lua
+++ Automatic-Arrows/Data/Automatic-Arrows/utils.lua
@@ -1,5 +1,18 @@
+AA.utils = {
+    distanceSettings = {
+        Long = { 600, 300, 3},
+        Medium = {300, 150, 3},
+        Close = {200, 75, 3}
+    },
+    currentSetting = "Long"
+}
+
+AA.utils.currentSetting = Storage:ReadValue("automaticarrows", "currentSetting")
+if (AA.utils.currentSetting == nil) then AA.utils.currentSetting = "Long" end
+
 function AA_InterpolateColorByDistance(distance)
     -- Calculate the RGB components based on the mapped value
+    local distanceLimits = AA.utils.distanceSettings[AA.utils.currentSetting]
     local far = I:Color(179, 16, 11)
     local mid = I:Color(214, 164, 13)
     local on = I:Color(70, 214, 13)
@@ -7,17 +20,17 @@ function AA_InterpolateColorByDistance(distance)
     local green = 0
     local blue = 0
 
-    if distance >= 600 then
+    if distance >= distanceLimits[1] then
         red = far.R
         green = far.G
         blue = far.B
-    elseif distance >= 200 then
-        local mappedValue = 1 - (distance - 300) / 300
+    elseif distance >= distanceLimits[2] then
+        local mappedValue = 1 - (distance - distanceLimits[2]) / (distanceLimits[1] - distanceLimits[2])
         red = far.R + ((mid.R - far.R) * mappedValue)
         green = far.G + ((mid.G - far.G) * mappedValue)
         blue = far.B + ((mid.B - far.B) * mappedValue)
-    elseif distance > 10 then
-        local mappedValue = distance / 300
+    elseif distance > distanceLimits[3] then
+        local mappedValue = distance / distanceLimits[2]
         red = on.R + ((mid.R - on.R) * mappedValue)
         green = on.G + ((mid.G - on.G) * mappedValue)
         blue = on.B + ((mid.B - on.B) * mappedValue)
